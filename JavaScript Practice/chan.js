27dd75a7090edaa8c327c71cd7b12a95403f6fa5
leechan1234 <124230969+leechan1234@users.noreply.github.com>
2023-02-14 21:50:45 +0900

    javascript 변수
    23-02-14 first jas study

---
+++ JavaScript Practice/chan.js
@@ -0,0 +1,35 @@
+let 변수이름 = 테이터;
+//변수가 생성되고 나면 변수(변수이름)를 지정된 데이터 대신 사용할 수 있다
+
+!특수기호 @_  공백x 
+
+const는 변수 재선언, 변수 재할당 모두 불가능하다.
+
+
+
+let message = 'Hello!'; // 변수를 정의하고 값을 할당합니다.
+
+alert(message); // Hello!
+console.log(message)
+
+
+
+
+
+let message;
+
+message = 'Hello!';
+
+message = 'World!'; // 값이 변경되었습니다.
+
+alert(message);
+
+let 변수이름;      //변수선언
+변수이름 = 데이터; // 변수 초기화
+let: '변수를 만들겠다'라는 의미
+
+
+
+let 변수이름;      //변수선언
+변수이름 = 데이터; // 변수 초기화
+let: '변수를 만들겠다'라는 의미
